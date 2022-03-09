2a8f39630a57c20b32ae333459d1072f8ec075a5
Tomek Regulski <tomek.regulski@gmail.com>
2022-03-09 07:07:40 -0500

    Add basic reward distribution and support for multiple tokens and users

--- programs/xfer/src/lib.rs
+++ programs/xfer/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
+use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, MintTo, TokenAccount, Transfer};
 use spl_token::instruction::AuthorityType;
 
 declare_id!("7xhwkYESJV7nhXfh3GwgE7rBvUMgo1oibhfYhwMSojd4");
@@ -19,6 +19,8 @@ pub mod xfer {
             .to_account_info()
             .key;
         ctx.accounts.escrow_account.initializer_amount = initializer_amount;
+        ctx.accounts.escrow_account.escrow_pk = *ctx.accounts.escrow_account_pk.to_account_info().key;
+        ctx.accounts.escrow_account.mint = *ctx.accounts.mint.to_account_info().key;
 
         let (vault_authority, _vault_authority_bump) =
             Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
@@ -35,7 +37,34 @@ pub mod xfer {
         Ok(())
     }
 
+    pub fn distribute(ctx: Context<Distribute>, count: u64) -> ProgramResult {
+        let mint_amount = 5;
+        let volume: u64 = mint_amount * count;
+        token::mint_to(ctx.accounts.into_mint_to_staker(), volume).unwrap();
+
+        Ok(())
+    }
+
     pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
+        
+        // let mint = "9agr4P3EJ82iJn3vAr9YdfVmfDWgQSrSZMe3UxEDzSpY".as_bytes();
+        // // let mint2 = b"9agr4P3EJ82iJn3vAr9YdfVmfDWgQSrSZMe3UxEDzSpY";
+        // let (_vault_authority, vault_authority_bump) =
+        //     Pubkey::find_program_address(&[ctx.accounts.mint.key().as_ref(), ESCROW_PDA_SEED], ctx.program_id);
+        // let authority_seeds = &[&ESCROW_PDA_SEED[..], &mint[..], &[vault_authority_bump]];
+
+        // token::transfer(
+        //     ctx.accounts
+        //         .into_transfer_to_initializer_context()
+        //         .with_signer(&[&authority_seeds[..]]),
+        //     ctx.accounts.escrow_account.initializer_amount,
+        // )?;
+
+        // token::close_account(
+        //     ctx.accounts
+        //         .into_close_context()
+        //         .with_signer(&[&authority_seeds[..]]),
+        // )?;
         let (_vault_authority, vault_authority_bump) =
             Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
         let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
@@ -66,7 +95,7 @@ pub struct Initialize<'info> {
     pub mint: Account<'info, Mint>, // The account of token account for token exchange. To be stored in EscrowAccount
     #[account(
         init,
-        seeds = [b"vault".as_ref()],
+        seeds = [b"vault".as_ref(), mint.key().as_ref()],
         bump,
         payer = initializer,
         token::mint = mint,
@@ -81,6 +110,8 @@ pub struct Initialize<'info> {
     // pub initializer_receive_token_account: Account<'info, TokenAccount>, // The account of EscrowAccount
     #[account(zero)]
     pub escrow_account: Box<Account<'info, EscrowAccount>>, // The account of Vault, which is created by Anchor via constraints.
+    /// CHECK:: checking
+    pub escrow_account_pk: AccountInfo<'info>,
     /// CHECK: checking
     pub system_program: AccountInfo<'info>,
     pub rent: Sysvar<'info, Rent>,
@@ -89,10 +120,37 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
+pub struct Distribute<'info> {
+    /// CHECK: checking
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+    /// CHECK: checking
+    #[account(mut)]
+    pub mint: AccountInfo<'info>,
+    /// CHECK: checking
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    /// CHECK: checking
+    pub token_program: AccountInfo<'info>,
+}
+
+impl<'info> Distribute<'info> {
+    fn into_mint_to_staker(&self) -> CpiContext<'_, '_, '_, 'info, MintTo<'info>> {
+        let cpi_accounts = MintTo {
+            mint: self.mint.to_account_info().clone(),
+            to: self.to.to_account_info().clone(),
+            authority: self.authority.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
 pub struct Cancel<'info> {
     /// CHECK: checking
     #[account(mut, signer)] 
     pub initializer: AccountInfo<'info>, // The initializer of EscrowAccount
+    pub mint: Account<'info, Mint>, // The account of token account for token exchange. To be stored in EscrowAccount
     #[account(mut)] // Marks the account as mutable and persists the state transition
     pub vault_account: Account<'info, TokenAccount>, // The program derived address
     /// CHECK: checking
@@ -116,7 +174,10 @@ pub struct EscrowAccount {
     pub initializer_deposit_token_account: Pubkey, // To record the deposit account of initialzer
     // pub initializer_receive_token_account: Pubkey, // To record the receiving account of initializer
     pub initializer_amount: u64, // To record how much token should the initializer transfer to taker
+    pub escrow_pk: Pubkey,
+    pub mint: Pubkey,
 }
+
 
 impl<'info> Initialize<'info> {
     fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
